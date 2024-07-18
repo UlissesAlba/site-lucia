0d06a0148afaba9ab84b06b59f99415f1dcd47ed
Ulisses <ualbardeiro@gmail.com>
2024-07-17 21:28:03 -0300

    modal menu

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,9 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
-    }
+
+
+function openModal() {
+    document.querySelector('.modal').classList.remove('hidden');
+}
+
+function closeModal() {
+    document.querySelector('.modal').classList.add('hidden');
 }
