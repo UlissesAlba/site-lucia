e090e0df6e3d4b63318fb6227e432cab5a228219
Ulisses <ualbardeiro@gmail.com>
2024-07-23 20:47:16 -0300

    finalização modal do menu

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,25 @@
-
-
-function openModal() {
+document.getElementById('open-modal').addEventListener('click', function(event) {
+    event.preventDefault();
     document.querySelector('.modal').classList.remove('hidden');
-}
+});
 
-function closeModal() {
+document.getElementById('close-modal').addEventListener('click', function() {
     document.querySelector('.modal').classList.add('hidden');
-}
+});
+
+
+/* function openModal() {
+    document.querySelector('.modal').classList.remove('hidden');
+} */
+
+
+/* function openModal() {
+    let menuMobile = document.querySelector('.modal');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+} */
